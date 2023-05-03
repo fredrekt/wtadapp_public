65246551420773c39bb6a744c0e06e302f4b3de5
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-03 20:42:54 +0800

    settings page init

--- frontend/src/pages/settings/index.tsx
+++ frontend/src/pages/settings/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './index.scss';
 import AdminLayout from '../../layouts/admin';
+import { Button, Card, Col, Row, Typography } from 'antd';
 
 const SettingsPage: React.FC = () => {
+	const onSave = async () => {
+		
+	}
 	return (
 		<AdminLayout className="settingsPage">
-			<div>1</div>
+			<div className="settingsContent">
+				<Row>
+					<Col span={10}>
+						<Card>
+							<Typography.Title level={4}>Settings</Typography.Title>
+							<Button onClick={onSave} type='primary'>Save</Button>
+						</Card>
+					</Col>
+				</Row>
+			</div>
 		</AdminLayout>
 	);
 };
