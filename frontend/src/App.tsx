c06c85192e527096cdb33b31520e3c7e07d9a29c
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-03 18:38:43 +0800

    added routes (new pages)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,12 @@ import { Route, Routes } from 'react-router-dom';
 import DashboardPage from './pages/dashboard';
 import ProtectedRoute from './utils/ProctectedRoute';
 import RegisterPage from './pages/register';
+import CustomersPage from './pages/customers';
+import GeographicPage from './pages/geographic';
+import CardFormatPage from './pages/cardFormat';
+import PageFormatPage from './pages/pageFormat';
+import SettingsPage from './pages/settings';
+import HelpPage from './pages/help';
 
 const App: React.FC = () => {
 	return (
@@ -18,6 +24,54 @@ const App: React.FC = () => {
 					element={
 						<ProtectedRoute>
 							<DashboardPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="customers"
+					element={
+						<ProtectedRoute>
+							<CustomersPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="card-format"
+					element={
+						<ProtectedRoute>
+							<CardFormatPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="page-format"
+					element={
+						<ProtectedRoute>
+							<PageFormatPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="geographic"
+					element={
+						<ProtectedRoute>
+							<GeographicPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="settings"
+					element={
+						<ProtectedRoute>
+							<SettingsPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="help"
+					element={
+						<ProtectedRoute>
+							<HelpPage />
 						</ProtectedRoute>
 					}
 				/>
