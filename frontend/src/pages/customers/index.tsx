ab8d0aa4fccbf06497ebced55503f54022be74ff
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-07 00:31:10 +0800

    data tables for customers list done

--- frontend/src/pages/customers/index.tsx
+++ frontend/src/pages/customers/index.tsx
@@ -1,11 +1,191 @@
 import React from 'react';
 import './index.scss';
 import AdminLayout from '../../layouts/admin';
+import { Button, Card, Col, Row, Select, Table, Typography, Space } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import {
+	PhoneOutlined,
+	MessageOutlined,
+	WechatOutlined
+} from '@ant-design/icons';
+
+interface DataType {
+	key: string;
+	name: string;
+	points: number;
+}
 
 const CustomersPage: React.FC = () => {
+	const columns: ColumnsType<DataType> = [
+		{
+			title: '#',
+			dataIndex: 'key',
+			key: 'id',
+			render: (text) => <span className='textPrimary'>#{text}</span>,
+			width: 50
+		},
+		{
+			title: 'Name',
+			dataIndex: 'name',
+			key: 'name',
+			render: (text) => <span className='textPrimary'>{text}</span>,
+			width: 200
+		},
+		{
+			title: 'Points',
+			dataIndex: 'points',
+			key: 'points',
+			render: (text) => <span className='textPrimary'>{text} Points</span>,
+			width: 100
+		},
+		{
+			title: '',
+			key: 'action',
+			render: (_, record) => (
+				<Space size="middle">
+					<WechatOutlined />
+					<PhoneOutlined />
+					<MessageOutlined />
+				</Space>
+			),
+			width: 500,
+			align: 'right',
+			className: 'tableActionCtas'
+		}
+	];
+
+	const data: DataType[] = [
+		{
+			key: '1',
+			name: 'John Brown',
+			points: 32,
+		},
+		{
+			key: '2',
+			name: 'Jim Green',
+			points: 42,
+		},
+		{
+			key: '3',
+			name: 'Joe Black',
+			points: 32,
+		},
+		{
+			key: '1',
+			name: 'John Brown',
+			points: 32,
+		},
+		{
+			key: '2',
+			name: 'Jim Green',
+			points: 42,
+		},
+		{
+			key: '3',
+			name: 'Joe Black',
+			points: 32,
+		},
+		{
+			key: '1',
+			name: 'John Brown',
+			points: 32,
+		},
+		{
+			key: '2',
+			name: 'Jim Green',
+			points: 42,
+		},
+		{
+			key: '3',
+			name: 'Joe Black',
+			points: 32,
+		},
+		{
+			key: '1',
+			name: 'John Brown',
+			points: 32,
+		},
+		{
+			key: '2',
+			name: 'Jim Green',
+			points: 42,
+		},
+		{
+			key: '3',
+			name: 'Joe Black',
+			points: 32,
+		},
+	];
+
 	return (
 		<AdminLayout className="customersPage">
-			<div>1</div>
+			<div className="customerPageContent">
+				<Row className="pageHeaderCta" align={'middle'} justify="end">
+					<Col span={2}>
+						<Button type="primary">Export</Button>
+					</Col>
+				</Row>
+
+				<Card className="customerCardFilter">
+					<Row gutter={[16, 16]} className="pageHeaderFilters" align={'middle'}>
+						<Col span={12}>
+							<Typography.Title className="filterTitle" level={4}>
+								Customers
+							</Typography.Title>
+						</Col>
+						<Col span={4}>
+							<Select
+								className="filterSelect"
+								placeholder={'A-Z'}
+								options={[
+									{
+										value: 'A-Z'
+									},
+									{
+										value: 'Z-A'
+									},
+									{
+										value: 'New'
+									},
+									{
+										value: 'Old'
+									}
+								]}
+							/>
+						</Col>
+						<Col span={4}>
+							<Select
+								className="filterSelect"
+								placeholder={'Higher'}
+								options={[
+									{
+										value: 'A-Z'
+									}
+								]}
+							/>
+						</Col>
+						<Col span={4}>
+							<Select
+								className="filterSelect"
+								placeholder={'Man'}
+								options={[
+									{
+										value: 'A-Z'
+									}
+								]}
+							/>
+						</Col>
+					</Row>
+				</Card>
+
+				<Card className='customerCardTable'>
+					<Row>
+						<Col span={24}>
+							<Table columns={columns} dataSource={data} />
+						</Col>
+					</Row>
+				</Card>
+			</div>
 		</AdminLayout>
 	);
 };
