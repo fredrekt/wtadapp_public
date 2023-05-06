a314a064fee649587e9b1848c5659708394dde72
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-06 17:44:49 +0800

    settings done

--- frontend/src/pages/settings/index.tsx
+++ frontend/src/pages/settings/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './index.scss';
 import AdminLayout from '../../layouts/admin';
-import { Button, Card, Col, Row, Typography } from 'antd';
+import { Button, Card, Col, Row, Typography, Form, Input } from 'antd';
 
 const SettingsPage: React.FC = () => {
-	const onSave = async () => {
+	const onFinish = async () => {
 		
 	}
 	return (
@@ -13,8 +13,60 @@ const SettingsPage: React.FC = () => {
 				<Row>
 					<Col span={10}>
 						<Card>
-							<Typography.Title level={4}>Settings</Typography.Title>
-							<Button onClick={onSave} type='primary'>Save</Button>
+							<Typography.Title className='formTitle' level={4}>Settings</Typography.Title>
+							<Form
+								className='settingsFormContainer'
+								name="basic"
+								labelCol={{ span: 8 }}
+								labelAlign='left'
+								wrapperCol={{ span: 16 }}
+								style={{ maxWidth: 600 }}
+								initialValues={{ remember: true }}
+								onFinish={onFinish}
+								autoComplete="off"
+								colon={false}
+							>
+								<Form.Item
+									label="Username"
+									name="username"
+								>
+									<Input />
+								</Form.Item>
+
+								<Form.Item
+									label="Business name"
+									name="username"
+								>
+									<Input />
+								</Form.Item>
+
+								<Form.Item
+									className='formButton'
+									label="Change Password"
+								>
+									<Button type="primary">Change</Button>
+								</Form.Item>
+
+								<Form.Item
+									className='formButton'
+									label="Change Email"
+								>
+									<Button type="primary">Change</Button>
+								</Form.Item>
+
+								<Form.Item
+									className='formButton'
+									label="Change Mobile number"
+								>
+									<Button type="primary">Change</Button>
+								</Form.Item>
+
+								<Form.Item className='formCta' wrapperCol={{ span: 4 }}>
+									<Button type="primary" htmlType="submit">
+										Save
+									</Button>
+								</Form.Item>
+							</Form>
 						</Card>
 					</Col>
 				</Row>
