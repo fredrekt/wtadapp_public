e8ec8fb4c970fafeafbcd9f4a34138952c986415
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-06 23:25:08 +0800

    card format page done

--- frontend/src/pages/cardFormat/index.tsx
+++ frontend/src/pages/cardFormat/index.tsx
@@ -1,11 +1,131 @@
 import React from 'react';
 import './index.scss';
 import AdminLayout from '../../layouts/admin';
+import { Card, Col, Row, Typography, Form, Input, Button, QRCode } from 'antd';
+import greyBg from '../../assets/images/grey_bg.jpg';
 
 const CardFormatPage: React.FC = () => {
+	const onFinish = async () => {
+
+	}
 	return (
 		<AdminLayout className="cardFormatPage">
-			<div>1</div>
+			<div className='cardFormatContent'>
+				<Row gutter={[16 ,16]}>
+					<Col span={14}>
+						<Card>
+							<Typography.Title className='formTitle' level={4}>Card format</Typography.Title>
+							<Form
+								className='cardFormatFormContainer'
+								name="basic"
+								labelCol={{ span: 10 }}
+								labelAlign='left'
+								wrapperCol={{ span: 14 }}
+								initialValues={{ remember: true }}
+								onFinish={onFinish}
+								autoComplete="off"
+								colon={false}
+							>
+
+								<Form.Item
+									className='customFormRowInputContainer'
+									label={
+										<div className='customLabel'>
+											<Typography.Title level={5}>
+												Logo
+											</Typography.Title>
+											<Typography.Paragraph>
+												Your business logo will appear at the top of the card.
+											</Typography.Paragraph>
+										</div>
+									}
+									name="username"
+								>
+									<Button type="text">Upload</Button>
+								</Form.Item>
+
+								<Form.Item
+									className='customFormRowInputContainer'
+									label={
+										<div className='customLabel'>
+											<Typography.Title level={5}>
+												Image
+											</Typography.Title>
+											<Typography.Paragraph>
+												An image that will appear under your business logo.
+											</Typography.Paragraph>
+										</div>
+									}
+									name="username"
+								>
+									<Button type="text">Upload</Button>
+								</Form.Item>
+
+								<Form.Item
+									className='customFormRowInputContainer'
+									label={
+										<div className='customLabel'>
+											<Typography.Title level={5}>
+												Color
+											</Typography.Title>
+											<Typography.Paragraph>
+												Card color for your business.
+											</Typography.Paragraph>
+										</div>
+									}
+									name="username"
+								>
+									<Button className='colorPicker' type="text"></Button>
+								</Form.Item>
+
+								<Form.Item className='formCta' wrapperCol={{ span: 4 }}>
+									<div className="formCtaMulti">
+										<Button type="primary" htmlType="submit">
+											Preview
+										</Button>
+										<Button type="primary" htmlType="submit">
+											Save
+										</Button>
+									</div>
+								</Form.Item>
+							</Form>
+						</Card>
+					</Col>
+					<Col className='cardContent' span={10}>
+						<Card>
+							<div className="cardHeader">
+								<Typography.Title className='cardTitle' level={4}>LOGO</Typography.Title>
+								<div className="cardHeaderPoints">
+									<Typography.Title className='pointsHeader' level={5}>
+										POINTS
+									</Typography.Title>
+									<Typography.Paragraph className='pointsCount'>
+										3000
+									</Typography.Paragraph>
+								</div>
+							</div>
+							<div className="imgContainer">
+								<img className='pageFormatImg' src={greyBg} alt="placeholder under logo" />
+							</div>
+							<div className="cardInformation">
+								<Typography.Paragraph className='cardHolderPlaceholder'>CARD HOLDER</Typography.Paragraph>
+								<Typography.Paragraph className='cardHolderName'>John Wayne</Typography.Paragraph>
+							</div>
+							<div className="ctaFooter">
+								<QRCode
+									value={'qrValue'}
+									color={'#000000'}
+									style={{
+										background: `#ffffff`,
+										border: `none`,
+										borderRadius: `4px`
+									}}
+								/>
+							</div>
+						</Card>
+					</Col>
+				</Row>
+			</div>
 		</AdminLayout>
 	);
 };
