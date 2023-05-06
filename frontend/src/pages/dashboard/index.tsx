aeeb44536402347f256d3bf0d3743b61afafb74a
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-07 00:52:17 +0800

    dashboard page done

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -2,23 +2,201 @@ import React from 'react';
 import './index.scss';
 import AdminLayout from '../../layouts/admin';
 import QRCodeComponent from '../../components/qrCode/QRCode';
-import { Card, Statistic } from 'antd';
-import { ArrowUpOutlined } from '@ant-design/icons';
+import { Card, Col, Row, Space, Statistic, Table, Typography } from 'antd';
+import { ArrowUpOutlined, PhoneOutlined, MessageOutlined, WechatOutlined } from '@ant-design/icons';
+import type { ColumnsType } from 'antd/es/table';
+import { Line } from 'react-chartjs-2';
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend
+} from 'chart.js';
+
+interface DataType {
+	key: string;
+	name: string;
+	points: number;
+}
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const DashboardPage: React.FC = () => {
+	const columns: ColumnsType<DataType> = [
+		{
+			title: '#',
+			dataIndex: 'key',
+			key: 'id',
+			render: (text) => <span className="textPrimary">#{text}</span>,
+			width: 50
+		},
+		{
+			title: 'Name',
+			dataIndex: 'name',
+			key: 'name',
+			render: (text) => <span className="textPrimary">{text}</span>,
+			width: 200
+		},
+		{
+			title: 'Points',
+			dataIndex: 'points',
+			key: 'points',
+			render: (text) => <span className="textPrimary">{text} Points</span>,
+			width: 100
+		},
+		{
+			title: '',
+			key: 'action',
+			render: (_, record) => (
+				<Space size="middle">
+					<WechatOutlined />
+					<PhoneOutlined />
+					<MessageOutlined />
+				</Space>
+			),
+			width: 500,
+			align: 'right',
+			className: 'tableActionCtas'
+		}
+	];
+
+	const data: DataType[] = [
+		{
+			key: '1',
+			name: 'John Brown',
+			points: 32
+		},
+		{
+			key: '2',
+			name: 'Jim Green',
+			points: 42
+		},
+		{
+			key: '3',
+			name: 'Joe Black',
+			points: 32
+		},
+		{
+			key: '1',
+			name: 'John Brown',
+			points: 32
+		},
+		{
+			key: '2',
+			name: 'Jim Green',
+			points: 42
+		},
+		{
+			key: '3',
+			name: 'Joe Black',
+			points: 32
+		},
+		{
+			key: '1',
+			name: 'John Brown',
+			points: 32
+		},
+		{
+			key: '2',
+			name: 'Jim Green',
+			points: 42
+		},
+		{
+			key: '3',
+			name: 'Joe Black',
+			points: 32
+		},
+		{
+			key: '1',
+			name: 'John Brown',
+			points: 32
+		},
+		{
+			key: '2',
+			name: 'Jim Green',
+			points: 42
+		},
+		{
+			key: '3',
+			name: 'Joe Black',
+			points: 32
+		}
+	];
+
+	const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+	const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: 'top' as const
+			},
+			title: {
+				display: true,
+				text: 'Chart.js Line Chart'
+			}
+		}
+	};
+	const lineData = {
+		labels,
+		datasets: [
+			{
+				label: 'Dataset 1',
+				data: labels.map(() => 100),
+				borderColor: 'rgb(255, 99, 132)',
+				backgroundColor: 'rgba(255, 99, 132, 0.5)'
+			},
+			{
+				label: 'Dataset 2',
+				data: labels.map(() => 200),
+				borderColor: 'rgb(53, 162, 235)',
+				backgroundColor: 'rgba(53, 162, 235, 0.5)'
+			}
+		]
+	};
+
 	return (
 		<AdminLayout className="dashboardPage">
 			<div className="dashboardRow">
 				<div className="dashboardContent">
-					<Card bordered={false}>
-						<Statistic
-							title="Active"
-							value={11.28}
-							precision={2}
-							valueStyle={{ color: '#3f8600' }}
-							prefix={<ArrowUpOutlined />}
-							suffix="%"
-						/>
+						<Row gutter={[24, 24]}>
+							<Col span={12}>
+								<Card bordered={false}>
+									<Statistic
+										title="Active"
+										value={11.28}
+										precision={2}
+										valueStyle={{ color: '#3f8600' }}
+										prefix={<ArrowUpOutlined />}
+										suffix="%"
+									/>
+								</Card>
+							</Col>
+							<Col span={12}>
+								<Card bordered={false}>
+									<Statistic
+										title="Active"
+										value={11.28}
+										precision={2}
+										valueStyle={{ color: '#3f8600' }}
+										prefix={<ArrowUpOutlined />}
+										suffix="%"
+									/>
+								</Card>
+							</Col>
+						</Row>
+					<Card className="dashboardLineGraphChart">
+						<Line options={options} data={lineData} />
+					</Card>
+					<Card className="customerCardTable">
+						<Typography.Title className="textPrimary tableTitle" level={4}>
+							Last Customers
+						</Typography.Title>
+						<Table columns={columns} dataSource={data} />
 					</Card>
 				</div>
 				<QRCodeComponent />
