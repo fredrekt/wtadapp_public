e65112b9a82c29584ae3e62d89d56c3d561d96a8
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-09 04:08:29 +0800

    now responsive

--- src/pages/geographic/index.tsx
+++ src/pages/geographic/index.tsx
@@ -11,7 +11,7 @@ const GeographicPage: React.FC = () => {
 		<AdminLayout className="geographicPage">
 			<div className='geographicContent'>
 				<Row gutter={[16 ,16]}>
-					<Col span={14}>
+					<Col xs={24} sm={24} md={14} lg={14} xl={14} xxl={14}>
 						<Card className='fullHeightCard'>
 							<Typography.Title className='formTitle' level={4}>Geographic</Typography.Title>
 							<Form
@@ -50,7 +50,7 @@ const GeographicPage: React.FC = () => {
 							</Form>
 						</Card>
 					</Col>
-					<Col className='mapContent' span={10}>
+					<Col className='mapContent' xs={24} sm={24} md={10} lg={10} xl={10} xxl={10}>
 						<Card>
 							<Typography.Title className='mapTitle' level={4}>Map</Typography.Title>
 							<iframe title="sampleMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14934509.338896776!2d36.04889435051902!3d23.95618070781279!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15e7b33fe7952a41%3A0x5960504bc21ab69b!2sSaudi%20Arabia!5e0!3m2!1sen!2sph!4v1683368418707!5m2!1sen!2sph" height="450" style={{ border: 0, width: `100%` }} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
