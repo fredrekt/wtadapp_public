c9a469ac4f9f6b6f979fe2ab31250c54425d2793
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-12 19:32:48 +0800

    added app logo to public footer

--- src/layouts/public.tsx
+++ src/layouts/public.tsx
@@ -1,6 +1,7 @@
 import { Layout } from 'antd';
 import { Content } from 'antd/es/layout/layout';
 import React from 'react';
+import AppLogoFooter from '../components/appLogo/AppLogoFooter';
 
 interface PublicLayoutProps {
 	className: string;
@@ -11,6 +12,7 @@ const PublicLayout: React.FC<PublicLayoutProps> = ({ children, className }) => {
 	return (
 		<Layout className={`wtdLayout ${className}`}>
 			<Content>{children}</Content>
+			<AppLogoFooter/>
 		</Layout>
 	);
 };
