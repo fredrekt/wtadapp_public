2b92899d66927d215ee915ba2ae8e5f08d20683b
Fredrick John Garingo <fredrickjohng6@gmail.com>
2023-05-12 09:56:20 +0800

    added white logo

--- src/layouts/admin.tsx
+++ src/layouts/admin.tsx
@@ -3,6 +3,7 @@ import Sider from 'antd/es/layout/Sider';
 import { Content } from 'antd/es/layout/layout';
 import React from 'react';
 import SideNav from '../components/sidenav/SideNav';
+import appLogo from '../assets/images/logo_white.png'
 
 interface AdminLayoutProps {
 	className: string;
@@ -13,6 +14,9 @@ const AdminLayout: React.FC<AdminLayoutProps> = ({ children, className }) => {
 	return (
 		<Layout className={`wtdAdminLayout ${className}`}>
 			<Sider
+				style={{
+					backgroundColor: `#401BFF`
+				}}
 				breakpoint="lg"
 				collapsedWidth="0"
 				onBreakpoint={(broken) => {
@@ -22,6 +26,7 @@ const AdminLayout: React.FC<AdminLayoutProps> = ({ children, className }) => {
 					console.log(collapsed, type);
 				}}
 			>
+				<img src={appLogo} alt="logo" className="appLogoSidenav" />
 				<SideNav />
 			</Sider>
 			<Content>{children}</Content>
