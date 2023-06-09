14b426183ca4b159efc0ec509b34abea559a0faa
Fred <fgaringo@ylopo.com>
2023-06-09 12:35:42 +0800

    added confirm setup plan

--- src/components/setupSelection/SetupSelection.tsx
+++ src/components/setupSelection/SetupSelection.tsx
@@ -1,6 +1,8 @@
-import { Card, Typography } from 'antd';
+import { Card, Typography, Modal } from 'antd';
 import React, { useState } from 'react';
 import './SetupSelection.scss';
+
+const { confirm } = Modal;
 
 interface SetupSelectionProps {
 	activeSetup: number;
@@ -11,17 +13,51 @@ interface SetupSelectionProps {
 	imgSrcHovered: string;
 }
 
-const SetupSelection: React.FC<SetupSelectionProps> = ({ activeSetup, currentSetup, label, imgSrc, imgSrcHovered, onClick }) => {
+const SetupSelection: React.FC<SetupSelectionProps> = ({
+	activeSetup,
+	currentSetup,
+	label,
+	imgSrc,
+	imgSrcHovered,
+	onClick
+}) => {
 	const [hovered, setHovered] = useState<boolean>(false);
+
+	const showConfirm = () => {
+		confirm({
+			title: 'Continue setup plan',
+			content: currentSetup === 1 ? 'For every riyal spent, they will earn points.' : 'Each time a customer visits, they will receive a stamp.',
+			centered: true,
+			okText: 'Accept',
+			cancelText: 'Cancel',
+			onOk() {
+				console.log('OK');
+				onClick(currentSetup);
+			},
+			onCancel() {
+				console.log('Cancel');
+			}
+		});
+	};
+
 	return (
 		<Card
 			onMouseEnter={() => setHovered(true)}
 			onMouseLeave={() => setHovered(false)}
-			onClick={() => onClick(currentSetup)}
+			onClick={showConfirm}
 			className={`setupSelectionCard ${currentSetup === activeSetup ? 'selected' : ''}`}
 		>
 			<div className="setupSelectionAsset">
-				<img src={hovered ? imgSrcHovered : currentSetup === activeSetup && (!hovered || hovered) ? imgSrcHovered : imgSrc} alt="points" />
+				<img
+					src={
+						hovered
+							? imgSrcHovered
+							: currentSetup === activeSetup && (!hovered || hovered)
+							? imgSrcHovered
+							: imgSrc
+					}
+					alt="points"
+				/>
 			</div>
 			<Typography.Text className="setupSelectionLabel">{label}</Typography.Text>
 		</Card>
